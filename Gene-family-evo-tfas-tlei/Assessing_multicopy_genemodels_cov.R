4e1f52f7ee686a28be658a9288f8d2843eed2015
clara <clara.groot.crego@univie.ac.at>
2021-07-27 17:21:30 +0200

    Update Assessing_multicopy_genemodels_cov.R

--- Gene-family-evo-tfas-tlei/Assessing_multicopy_genemodels_cov.R
+++ Gene-family-evo-tfas-tlei/Assessing_multicopy_genemodels_cov.R
@@ -7,16 +7,24 @@
 
 library(ggplot2)
 library(plyr)
+library(dplyr)
 library(wesanderson)
 
 # Set colours categories
 mycolors <- c(wes_palette("Darjeeling1")[1], wes_palette("Darjeeling1")[2], wes_palette("FantasticFox1")[3],
               wes_palette("Darjeeling1")[3], wes_palette("Rushmore1")[3], wes_palette("GrandBudapest1")[3])
 
-setwd("/Users/clara/bio-info_phd/Comparative_genomics/Gene_family_evolution/")
+setwd("/home/clara/Documents/GitHub/Tillandsia-compgenomics/Gene-family-evo-tfas-tlei/")
 tfas_genes <- read.table("Tfas_pergene_mediancov_and_orthoinfo.txt", header = T)
-mean(tfas_genes$median_cov) # 47.3
-mean(tfas_genes$mean_cov) # 47.9
+
+# Removing all genes from chloroplastic, ribosomal or mitochondrial genes
+mito_og <- read.table("mito_plastid_ribo_OGs.txt", header = F, sep = "\t")
+colnames(mito_og) <- c("og_id")
+tfas_no_mito <- tfas_genes %>%
+  filter(!(og_id %in% mito_og$og_id))
+
+mean(tfas_genes$median_cov) # 47.5
+mean(tfas_genes$mean_cov) # 48.04
 
 # Divide genes into categories based on copy number
 dup <- c()
@@ -57,6 +65,29 @@ tfas_genes$duplicated <- dup
 # Obtain average coverage for each category
 mu <- ddply(tfas_genes, "dup", summarise, grp.mean=mean(median_cov))
 head(mu)
+# dup grp.mean
+# ancestral-multi-copy    40.28850
+# ancestral-single-copy   42.91895
+# multi-copy              42.65542
+# single-copy             65.12410
+# unique-multi-copy       52.45243
+
+
+#Statistics for the full genome
+# count    6.002822e+08
+# mean     46.17124
+# std      110.3986
+# min      0
+# 25%      21.00000
+# 50%      36.00000
+# 75%      51.00000
+# max      8070.000
+
+sd(tfas_no_mito[tfas_no_mito$duplicated == "ancestral-single-copy", 3])
+mean(tfas_no_mito[tfas_no_mito$duplicated == "ancestral-single-copy", 3])
+quantile(tfas_no_mito[tfas_no_mito$duplicated == "ancestral-single-copy", 3], c(.01, .05, .1, .25, .5,.75, .9, .95, .99)) 
+#1%       5%      10%      25%      50%      75%      90%      95%      99% 
+#15.10657 22.41436 27.21449 34.50790 39.82849 43.67525 46.54118 48.44374 69.23726
 
 # Set colors for simple categories
 mycolors <- c(wes_palette("Darjeeling1")[1], wes_palette("Darjeeling1")[2], wes_palette("FantasticFox1")[3],
@@ -64,6 +95,13 @@ mycolors <- c(wes_palette("Darjeeling1")[1], wes_palette("Darjeeling1")[2], wes_
 
 # Density plot of mean coverage per gene per category
 ggplot(tfas_genes, aes(x=mean_cov, color=dup)) +
+  geom_density() +
+  xlim(0,150) +
+  geom_vline(xintercept = 46.1712, linetype = "longdash", colour = "gray28") +
+  scale_color_manual(values = mycolors) +
+  scale_fill_manual(values = mycolors)
+
+ggplot(tfas_no_mito, aes(x=mean_cov, color=duplicated)) +
   geom_density() +
   xlim(0,150) +
   geom_vline(xintercept = 46.1712, linetype = "longdash", colour = "gray28") +
@@ -78,7 +116,9 @@ ggplot(tfas_genes, aes(x=median_cov, color=dup)) +
   scale_color_manual(values = mycolors) +
   scale_fill_manual(values = mycolors)
 
-
+---------------
+# UPDATE 27.07: I skipped establishing a cutoff and corrected sizes for all multicopy families
+#
 ### Establish a cutoff to separate "real" from "faulty" gene models
 ## Using the package "cutoff", we can establish a cut-off value that separates the bimodal
 ## distribution into two unimodal, normal distributions. The cut-off value represents the point
@@ -89,38 +129,72 @@ ggplot(tfas_genes, aes(x=median_cov, color=dup)) +
 #devtools::install_github("choisy/cutoff")
 #install.packages("cli")
 #install.packages("bbmle")
-library(cutoff)
+#library(cutoff)
 
-tfas_multicopy_mean_cov <- as.vector(tfas_genes[tfas_genes$duplicated == 'multi-copy', 3])
-tfas_multicopy_mean_cov_df <- tfas_genes[tfas_genes$duplicated == 'multi-copy',]
-tfas_multicopy_mean_cov_subset <- tfas_multicopy_mean_cov[tfas_multicopy_mean_cov < 100]
-tfas_multicopy_FMM <- em(tfas_multicopy_mean_cov_subset, 'normal', 'normal')
-hist(tfas_multicopy_mean_cov_subset,10000,F,xlim=c(0,100),xlab="median coverage",ylab="density",main=NULL,col="grey")
-lines(tfas_multicopy_FMM,lwd=1.5,col="red")
-abline(v=34.5,lty=2,col="lightblue")
+#tfas_multicopy_mean_cov <- as.vector(tfas_genes[tfas_genes$duplicated == 'multi-copy', 3])
+#tfas_multicopy_mean_cov_df <- tfas_genes[tfas_genes$duplicated == 'multi-copy',]
+#tfas_multicopy_mean_cov_subset <- tfas_multicopy_mean_cov[tfas_multicopy_mean_cov < 100]
+#tfas_multicopy_FMM <- em(tfas_multicopy_mean_cov_subset, 'normal', 'normal')
+#hist(tfas_multicopy_mean_cov_subset,10000,F,xlim=c(0,100),xlab="median coverage",ylab="density",main=NULL,col="grey")
+#lines(tfas_multicopy_FMM,lwd=1.5,col="red")
+#abline(v=34.5,lty=2,col="lightblue")
 
-cut_off <- cutoff(tfas_multicopy_FMM)
-cut_off # Genes with mean cov < 34.5 belong to "faulty" category
+#cut_off <- cutoff(tfas_multicopy_FMM)
+#cut_off # Genes with mean cov < 34.5 belong to "faulty" category
 
-faulty_genes <- tfas_multicopy_mean_cov_df[tfas_multicopy_mean_cov_df$mean_cov < 34.5,] # 4753 genes
-table(faulty_genes$Tfas_count)
+#faulty_genes <- tfas_multicopy_mean_cov_df[tfas_multicopy_mean_cov_df$mean_cov < 34.5,] # 4753 genes
+#table(faulty_genes$Tfas_count)
 
 # Recording all genes with high coverage to explore further (mostly mitochondrial and plastid)
 high_cov_genes <- tfas_genes[tfas_genes$mean_cov > 100,]
 write.table(high_cov_genes, file = "100x_genes_Tfas.txt")
 
 # Recording all genes with a mean coverage < 34.5 to correct gene family sizes
-write.table(faulty_genes, file = "faulty_gene_families_35x.txt")
-
+#write.table(faulty_genes, file = "faulty_gene_families_35x.txt")
+-----------------
+  
 # Correcting the gene family sizes based on coverage differences
 # Select all orthogroups containing faulty genes
-library(dplyr)
-orthogroups_w_faulty_genes <- tfas_genes %>%
-  filter(og_id %in% faulty_genes$og_id)
+# library(dplyr)
+# orthogroups_w_faulty_genes <- tfas_genes %>%
+#  filter(og_id %in% faulty_genes$og_id)
+# This resulted in 6861 genes and 1981 orthogroups
+
+# Now I select ALL multicopy genes - this immediately captures the full orthogroup as well
+tfas_multicopy_genes <- tfas_genes[tfas_genes$duplicated == 'multi-copy',] # 8400 genes, 2632 orthogroups
+tfas_no_mito_multicopy_genes <- tfas_no_mito[tfas_no_mito$duplicated == 'multi-copy',] # 7348 genes, 2353 orthogroups
+
+lower_thresh=34.5079# 25th quantile of the average coverage of ancestral single copy genes to accont for variation
+upper_thresh=43.6752 # 75th quantile of the average coverage of ancestral single copy genes
+corr_Tfas_no_mito <- data.frame()
+for (i in unique(tfas_no_mito_multicopy_genes$og_id)){
+  orthogroup <- tfas_no_mito_multicopy_genes[tfas_no_mito_multicopy_genes$og_id == i,]
+  nr_genes <- as.integer(orthogroup[1,6])
+  total_mean_cov <- sum(orthogroup$mean_cov)
+  lower_thresh_total <- nr_genes * lower_thresh
+  upper_thresh_total <- nr_genes * upper_thresh
+  expected_mean_cov <- nr_genes * 42.9189
+  correction_factor <- total_mean_cov/expected_mean_cov
+  new_size = round((nr_genes * correction_factor), digits = 0)
+  if (lower_thresh_total < total_mean_cov && total_mean_cov < upper_thresh_total){
+    new_size = nr_genes
+  }
+  #if (correction_factor > 1){
+  #  new_size = nr_genes
+  #}
+  if (total_mean_cov < 42.9189){
+    new_size = 1
+  }
+  corr_family_size <- cbind(i, as.numeric(correction_factor), as.integer(new_size),
+                            as.integer(nr_genes), as.numeric(total_mean_cov), expected_mean_cov, 
+                            lower_thresh_total,upper_thresh_total)
+  corr_Tfas_no_mito <- rbind(corr_Tfas_no_mito, corr_family_size)
+}
+colnames(corr_Tfas_no_mito) <- c("og_id", "correction_factor", "corr_Tfas_count","old_Tfas_count", "total_mean_cov", "expected_mean_cov", "Lower_thresh", "Upper_thresh")
 
 corr_Tfas <- data.frame()
-for (i in unique(orthogroups_w_faulty_genes$og_id)){
-  orthogroup <- orthogroups_w_faulty_genes[orthogroups_w_faulty_genes$og_id == i,]
+for (i in unique(tfas_multicopy_genes$og_id)){
+  orthogroup <- tfas_multicopy_genes[tfas_multicopy_genes$og_id == i,]
   nr_genes <- as.integer(orthogroup[1,6])
   total_mean_cov <- sum(orthogroup$mean_cov)
   expected_mean_cov <- nr_genes * 46.1712
@@ -136,8 +210,14 @@ for (i in unique(orthogroups_w_faulty_genes$og_id)){
                             as.integer(nr_genes), as.numeric(total_mean_cov), expected_mean_cov)
   corr_Tfas <- rbind(corr_Tfas, corr_family_size)
 }
-rownames(corr_Tfas) <- c(1:1981)
-colnames(corr_Tfas) <- c("og_id", "correction_factor", "corr_Tfas_count","old_Tfas_count", "total_mean_cov", "expected_mean_cov")
+colnames(corr_Tfas) <- c("og_id", "correction_factor", "corr_Tfas_count","old_Tfas_count",
+                         "total_mean_cov", "expected_mean_cov")
+
+
+increase <- corr_Tfas[as.integer(corr_Tfas$corr_Tfas_count) > as.integer(corr_Tfas$old_Tfas_count),]
+increase_plastid <- increase %>%
+  filter(!(og_id %in% mito_og$og_id))
+
 
 write.table(corr_Tfas, file = "corrected_family_sizes_Tfas.txt")
 
