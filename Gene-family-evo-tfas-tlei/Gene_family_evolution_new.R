42a5653fe9029b327621ef0e95bd3bbf45f4de80
cgrootcrego <clara.groot.crego@univie.ac.at>
2021-08-27 16:07:16 +0200

    Update Gene_family_evolution_new.R

--- Gene-family-evo-tfas-tlei/Gene_family_evolution_new.R
+++ Gene-family-evo-tfas-tlei/Gene_family_evolution_new.R
@@ -9,24 +9,23 @@ colnames(counts) <- c("og_id", "Acom", "Tfas", "Tlei")
 
 # Filter out unique orthogroups
 counts_Tfas_Tlei <- counts[counts$Tfas != 0 & counts$Tlei != 0,]
-ggplot(counts_Tfas_Tlei, aes(x=Tfas, y=Tlei)) + geom_point(size = .5)
-
-# Same without plastid annotations
-counts_noplastid <- read.table("orthogroups_Tfas_Tlei_Acom.counts.no_TEs.size_corrections-bothspecies.no_plastid-mito-ribo.txt",
-                               sep = "\t")
-colnames(counts_noplastid) <- c("og_id", "Acom", "Tfas", "Tlei")
-counts_Tfas_Tlei_npl <- counts_noplastid[counts_noplastid$Tfas != 0 & counts_noplastid$Tlei != 0,]
-counts_Tfas_Tlei_npl <- counts_Tfas_Tlei_npl[counts_Tfas_Tlei_npl$og_id != "OG0000015",]
-
-ggplot(counts_Tfas_Tlei_npl, aes(x=Tfas, y=Tlei)) + geom_point(size = .5) +
+ggplot(counts_Tfas_Tlei, aes(x=Tfas, y=Tlei)) + geom_point(size = .5) +
   geom_abline(intercept = 0, slope = 1) +
   labs(title = "Per-species gene counts in multi-copy orthogroups") +
   ylab(label = "T. leiboldiana") +
   xlab(label = "T. fasciculata") +
   theme_bw()
 
-counts_Tfas_Tlei_npl_multi <- counts_Tfas_Tlei_npl[!(counts_Tfas_Tlei_npl$Tfas == 1 & counts_Tfas_Tlei_npl$Tlei == 1),]
+# Add colour gradient to show number of datapoints with the same count combination
+counts_Tfas_Tlei_multi <- counts_Tfas_Tlei[!(counts_Tfas_Tlei$Tfas == 1 & counts_Tfas_Tlei$Tlei == 1),]
+ggplot(counts_Tfas_Tlei_multi) + geom_hex(aes(Tfas, Tlei), bins = 100) +
+  labs(title = "Per-species gene counts in multi-copy orthogroups") +
+  ylab(label = "T. leiboldiana") +
+  xlab(label = "T. fasciculata") +
+  scale_fill_continuous(type = "viridis") +
+  theme_bw()
+  theme_bw()
+
 write.table(counts_Tfas_Tlei_npl_multi, file = "orthogroup_selection_for_GO_term_all.txt", sep = "\t")
 
-chisq.test(counts_Tfas_Tlei_npl$Tfas, counts_Tfas_Tlei_npl$Tlei, correct=FALSE)
-  
+
