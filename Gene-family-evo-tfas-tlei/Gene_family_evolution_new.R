15ddfe5efe6b328b48128631b421ae2253c67d74
cgrootcrego <clara.groot.crego@univie.ac.at>
2021-09-06 14:03:01 +0200

    Update Gene_family_evolution_new.R

--- Gene-family-evo-tfas-tlei/Gene_family_evolution_new.R
+++ Gene-family-evo-tfas-tlei/Gene_family_evolution_new.R
@@ -4,7 +4,7 @@ library(ggplot2)
 
 setwd("Documents/GitHub/Tillandsia-compgenomics/Gene-family-evo-tfas-tlei/")
 setwd("/home/clara/Documents/GitHub/Tillandsia-compgenomics/Gene-family-evo-tfas-tlei/")
-counts <- read.table("orthogroups_Tfas_Tlei_Acom.counts.no_TEs.size_corrections.no_plastid-mito-ribo2.txt", sep = '\t')
+counts <- read.table("orthogroups_Tfas_Tlei_Acom.counts.with_functional_info.no_TEs.size_corrections.no_plastid-mito-ribo.blastandsearch.txt", sep = '\t')
 colnames(counts) <- c("og_id", "Acom", "Tfas", "Tlei")
 
 # Filter out unique orthogroups
@@ -26,6 +26,6 @@ ggplot(counts_Tfas_Tlei_multi) + geom_hex(aes(Tfas, Tlei), bins = 100) +
   theme_bw()
   theme_bw()
 
-write.table(counts_Tfas_Tlei_npl_multi, file = "orthogroup_selection_for_GO_term_all.txt", sep = "\t")
+write.table(counts_Tfas_Tlei_multi, file = "orthogroup_selection_multicopy_for_GO_term_all.txt", sep = "\t")
 
 
