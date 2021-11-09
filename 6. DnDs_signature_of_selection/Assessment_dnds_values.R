ae909c01fc023ed7ea14bfa333a9f78e56bd8eac
cgrootcrego <clara.groot.crego@univie.ac.at>
2021-11-09 17:15:59 +0100

    Update Assessment_dnds_values.R

--- 6. DnDs_signature_of_selection/Assessment_dnds_values.R
+++ 6. DnDs_signature_of_selection/Assessment_dnds_values.R
@@ -2,22 +2,24 @@ library(ggplot2)
 setwd("/Users/clara/Documents/GitHub/Tillandsia-compgenomics/6. DnDs_signature_of_selection/")
 
 dnds_all <- read.delim("pairwise_dnds.codeml.allgenes.output", sep = "\t", header = T)
-dnds_possel_sign <- dnds_all[dnds_all$dN.dS > 1 & dnds_all$pvalue < 0.05,]
+dnds_possel_sign <- dnds_all[dnds_all$dN.dS > 1 & dnds_all$pvalue <= 0.1,]
 dnds_possel <- dnds_all[dnds_all$dN.dS > 1,]
 dnds_sign <- dnds_all[dnds_all$pvalue < 0.05,]
 
 c <- c()
 for (i in 1:nrow(dnds_all)){
   if (dnds_all[i,8] <= 0.05){
-    c <- c(c, "sign")
+    c <- c(c, "p <= 0.05")
+  } else if (dnds_all[i,8] <= 0.1){
+    c <- c(c, "p <= 0.1")
   } else {
-    c <- c(c, "non")
+    c <- c(c, "p > 0.1")
   }
 }
 dnds_all$sign <- c
 
 ggplot(dnds_sign) + aes(x = dN.dS) + geom_density() + xlim(c(-1,3))
-ggplot(dnds_all) + aes(x = dN.dS, color = sign) + geom_density() + xlim(c(-1,3))
+ggplot(dnds_all) + aes(x = dN.dS, color = sign) + geom_density() + xlim(c(-1,5))
 
 mean(dnds_sign$dN.dS)
 mean(dnds_sign$dN)
